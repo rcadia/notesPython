0a6185bd90d042037031fa1b3741bc86e516ace9
ross <rossbendal@gmail.com>
2013-06-17 15:58:12 +0800

    Added modules and functions.
    First data to be uploaded using a github shell in Windows XP.

---
+++ Modules and Functions.py
@@ -0,0 +1,26 @@
+#
+# Tutorial - 4 
+# Modules and Functions
+#
+# Ross Alejandro A. Bendal
+# Monday - June 17, 2013 - 3:34
+#
+# Modules - are functions that needs to be imported.
+# Functions - are built in functions inside the compiler.
+
+
+########################## FUNCTIONS ######################################
+pow(5,4)            #Built-in function that gets the power in a parameter.pow(base,exponent.
+
+abs(-18)            #Absolute value.
+
+
+######################### MODULES #########################################
+
+import math         #Imports the math module.
+
+math.floor(18.7)    #Needs to type modulename first, then the function name.
+math.sqrt(81)       #Example of square root function inside math module.
+
+square = math.sqrt  #Imported functions can be assigned to a variable.
+square(9)
