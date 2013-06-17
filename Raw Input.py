2a8d4b409e8db54bdc140ba1675cd45ca3c77d34
ross <rossbendal@gmail.com>
2013-06-17 21:22:41 +0800

    Added Raw Input notes

---
+++ Raw Input.py
@@ -0,0 +1,13 @@
+#
+# Tutorial - 8
+# Raw Input
+#
+# Ross Alejandro A. Bendal
+# Monday - June 17, 2013 - 9:18PM
+#
+# 
+#input function treats the value and treats it as expression.
+#raw_input treats the user input as string.
+
+x = raw_input("Enter name: ")
+print x
