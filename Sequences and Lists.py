8f3dbef3b0f6f7b2b4367a90a8f39047b276bf61
ross <rossbendal@gmail.com>
2013-06-17 22:00:16 +0800

    Added Sequences and Lists

---
+++ Sequences and Lists.py
@@ -0,0 +1,20 @@
+#
+# Tutorial - 9
+# Sequences and Lists
+#
+# Ross Alejandro A. Bendal
+# Monday - June 17, 2013 - 9:48PM
+#
+# Lists are indexed.
+# Strings can be sequenced.
+
+########################## LISTS #############################
+pets = ['dog', 'cat', 'pig', 'bear'] #indexed
+print pets[2]
+print pets[-2] #can count from the last item in List starting from -1. Zero wont make any sense. -1
+
+
+########################## SEQUENCES #########################
+
+name = 'ross'
+print name[2] #can also count backwards. 
